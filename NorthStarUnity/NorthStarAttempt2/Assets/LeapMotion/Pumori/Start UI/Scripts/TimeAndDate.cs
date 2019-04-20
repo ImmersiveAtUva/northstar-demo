8312850a3dfceabe0ee461dd65d6242c0554c523
asdf <fdsa>
2019-04-20 17:15:44 -0400

    imported project

---
+++ NorthStarUnity/NorthStarAttempt2/Assets/LeapMotion/Pumori/Start UI/Scripts/TimeAndDate.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TimeAndDate : MonoBehaviour
+{
+
+    public TextMeshPro currentTime;
+    public TextMeshPro currentDate;
+
+
+    
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime.text = System.DateTime.Now.ToString("hh:mm tt");
+        currentDate.text = System.DateTime.Today.DayOfWeek.ToString(); 
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
